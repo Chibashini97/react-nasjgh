44f7f5d68534cfa523d7668fd45f7c13e81a6b05
Chibashini97 <chibashini2014@gmail.com>
2019-12-18 19:08:51 +0530

    1 file modified

---
+++ reducer/LoginReducer.js
@@ -0,0 +1,12 @@
+import LOGIN_CLICK from '../../LoginActionType'
+
+function LoginReducer (state,action){
+  switch(action.type){
+    case LOGIN_CLICK : 
+      {return 
+      isDoneAuthetication : true};
+    default :
+    {return state}
+    
+  }
+}
