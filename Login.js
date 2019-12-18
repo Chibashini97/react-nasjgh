fadaafb25d9bd9244462387ea9ff43ca243d8de6
Chibashini97 <chibashini2014@gmail.com>
2019-12-18 18:33:45 +0530

    1 file modified

--- Login.js
+++ Login.js
@@ -16,14 +16,14 @@ export default class Login extends Component
 {constructor(props)
     {
       super(props)
-      this.state={name:'',email:'',password:'',nameError:'',emailError:'',passwordError:''}
+      this.state={name:'',password:'',nameError:'',passwordError:''}
       this.handleChange=this.handleChange.bind(this);
       this.handleSubmit=this.handleSubmit.bind(this);
     }
   handleChange(e)
   {
     this.setState({[e.target.name]:e.target.value});
-    this.setState({nameError:'',emailError:'',passwordError:''})
+    this.setState({nameError:'',passwordError:''})
   }
 
   handleSubmit(event)
@@ -31,7 +31,6 @@ export default class Login extends Component
     const result=this.validate();
     if(result)
     {
-        // this.setState({nameError:'',emailError:''})
         alert("name submitted");
        this.props.history.push('/Logout');
     }
@@ -43,25 +42,24 @@ export default class Login extends Component
   validate()
   {
       let nameError='';
-      let emailError='';
       let passwordError='';
       if(!this.state.name)
       {
         nameError="name cannot be empty";
       }
      
-      if(!this.state.email.includes("@"))
-      {
-        emailError="Invalid Email ID";
-      }
+      // if(!this.state.email.includes("@"))
+      // {
+      //   emailError="Invalid Email ID";
+      // }
 
       if(!this.state.password)
       {
         passwordError="Password can't be empty";
       }
-      if(emailError || nameError || passwordError)
+      if(nameError || passwordError)
       {
-          this.setState({emailError,nameError,passwordError})
+          this.setState({nameError,passwordError})
           return false;
       }
       return true;
@@ -78,12 +76,12 @@ export default class Login extends Component
       <div className="Decor">
           <div className="Log">LOGIN</div>
 
-        <b>Name:</b><br /><input type="text" name="name" onChange={this.handleChange} value={this.state.name}/>
+        <b>UserName:</b><br /><input type="text" name="name" onChange={this.handleChange} value={this.state.name}/>
         <div style={{color:'red'}}> {this.state.nameError} </div>
         <br/>
-        <b>Email ID:</b><br /><input type="text" name="email" onChange={this.handleChange} value={this.state.email}/>
-        <div style={{color:'red'}}> {this.state.emailError} </div>
-        <br />
+        // <b>Email ID:</b><br /><input type="text" name="email" onChange={this.handleChange} value={this.state.email}/>
+        // <div style={{color:'red'}}> {this.state.emailError} </div>
+        // <br />
         <b>Password:</b><br /><input type="password" name="password"  onChange={this.handleChange} value={this.state.password}/>
         <div style={{color:'red'}}> {this.state.passwordError} </div>
         <br />
