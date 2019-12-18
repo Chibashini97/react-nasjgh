35805d4f533e07521fab16ad06c802c377fc99cf
Chibashini97 <chibashini2014@gmail.com>
2019-12-18 09:46:31 +0530

    1 file modified

--- Display.js
+++ Display.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Login from "./Login";
 
 import Logout from "./Logout";
-import HelloRenu from "./HelloRenu";
 import { Route, NavLink, HashRouter } from "react-router-dom";
 
 class Display extends Component {
@@ -10,7 +9,6 @@ class Display extends Component {
     return (
       <HashRouter history={history}>
         <Route exact path="/" component={Login} />
-        <Route exact path="/HelloRenu" component={HelloRenu} />
         <Route exact path="/Logout" component={Logout} />
       </HashRouter>
     );
